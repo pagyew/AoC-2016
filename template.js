062608bb0cc2a66a84d8498fb1a8ecf43b96d0fa
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-04-03 13:46:22 +0600

    updates template

--- template.js
+++ template.js
@@ -2,13 +2,12 @@ const { readFileSync } = require('node:fs')
 const { basename } = require('node:path')
 
 const day = basename(__filename, '.js')
-const input = readFileSync(__dirname + `/${day}.txt`, 'utf8').trim()
-
-const n = input.length
+const input = readFileSync(__dirname + `/${day}.txt`, 'utf8').trim().split('\n')
 
 for (let part2 of [false,true]) {
   console.time(`day ${day} ${part2 ? 'part 2' : 'part 1'}`)
-
+  
+  const n = input.length
   let ans = 0
 
 
